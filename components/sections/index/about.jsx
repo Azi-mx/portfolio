e56571e9bb4c52ff8b05dc5f3ea243753a2754b3
azim308 <azim.gfuturetech@gmail.com>
2024-12-31 15:36:24 +0530

    Add initial project structure with configuration, layout, and styling components with about, technical section and hero and footer. The articles are fetched from medium and featured projects seperate component is made.

---
+++ components/sections/index/about.jsx
@@ -0,0 +1,72 @@
+// Core packages
+import Image from "next/image";
+
+// Section structure
+import Section from "../../structure/section";
+import Container from "../../structure/container";
+
+// Section general blocks
+import SectionTitle from "../../blocks/section.title.block";
+import SectionGridBg from "../../blocks/section.grid.block";
+
+// Section specific blocks
+import BadgesBlock from "../../blocks/about.badges.block";
+import CopyBlock from "../../blocks/about.copy.block";
+
+// Section scss
+import about from "../../../styles/sections/index/about.module.scss";
+
+/**
+ * Section: About
+ * An overview of yourself.
+ * Highlight your top level attributes and disciplines.
+ *
+ * @returns {jsx} <About />
+ */
+export default function About() {
+  return (
+    <Section classProp={about.section}>
+      <Container spacing={["verticalXXXLrg"]}>
+        <SectionTitle
+          title="About Me"
+          preTitle="Synopsis"
+          subTitle="With a mastery of UX design, UI design, full-stack development, AWS services, and database management, I transform ideas into captivating digital experiences. As a versatile digital creator, I am driven by a passion for continuous innovation and making a lasting impact through every project I undertake."
+        />
+        <section className={about.content}>
+          <div className={about.image}>
+            <img src="/img/AzimBagsariya.jpeg" alt="Azim Bagsariya" />
+          </div>
+          <div className={about.copy}>
+            <CopyBlock
+              title="Softskills that pay the bills"
+              containerClass={about.container}
+              iconClass={about.icon}
+              icon={["fat", "ear-listen"]}
+              copy="Beyond my design and technical prowess, I bring exceptional leadership, time management, and multitasking abilities to the table. I have successfully led diverse teams through complex projects, orchestrating each phase with precision and a keen eye for detail. My project management skills ensure that we hit every milestone on time and within budget, while fostering a collaborative and motivated team environment."
+            />
+            <BadgesBlock
+              title="Reasearch and planning"
+              containerClass={about.container}
+              list={methods}
+              fullContainer="fullContainer"
+              block="methods"
+              icon="fingerprint"
+              copy="One of my favorite aspects of creating is planning the architecture of a project. From Design Systems to Brand Strategy—I enjoy working with the many touch points of user experience."
+              //invertedColor="invertedColor"
+              headerIcon={`${about.icon}`}
+            />
+          </div>
+        </section>
+      </Container>
+    </Section>
+  );
+}
+
+const methods = [
+  { key: "planet-moon", name: "User Research", type: "fad" },
+  { key: "qrcode", name: "Digital Strategy", type: "fad" },
+  { key: "window", name: "Design Systems", type: "fad" },
+  { key: "cubes", name: "Product Strategy", type: "far" },
+  { key: "layer-plus", name: "Brand Strategy", type: "fad" },
+  { key: "solar-system", name: "Operations", type: "fad" },
+];
